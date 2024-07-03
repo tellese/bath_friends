1b4b3f1f1e97621e95d5f20404d5b91c401ef7b3
tellese <dariantellese@gmail.com>
2024-07-03 19:29:20 +0900

    세번째  커밋: 라이브러리 설치

--- src/component/App.js
+++ src/component/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <div style={{ minHeight: '100vh', width: '1440px' }}>
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
           <Routes>
             <Route path="/" element={<Landingpage />} />
             <Route path="/suggest" element={<Suggestions />} />
